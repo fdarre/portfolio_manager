be3013fdc85ca70b2476043b838177e13a89bd36
Frédéric Darré <17145256+fdarre@users.noreply.github.com>
2023-03-07 11:31:06 +0100

    Implements API call to get price data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AssetOverview from "./components/AssetOverview";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+
+    //state
+    const [fetchedPrice, setFetchedPrice] = useState<number>(0);
+
+    //behaviour
+    const FetchPromise = fetch('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT');
+    const dataPromise = FetchPromise.then((response) => response.json());
+    const price = dataPromise.then((data) => setFetchedPrice(data.price));
+
+
+    //render
+      return (
+        <div className="App">
+            <AssetOverview name="Ethereum" symbol="ETH" price={fetchedPrice} />
+         </div>
+      );
 }
 
 export default App;
