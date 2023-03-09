100cf86f8000ebe4ad1d6ff7fabcb49f2eb29727
Frédéric Darré <17145256+fdarre@users.noreply.github.com>
2023-03-09 11:30:25 +0100

    Deletes unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import AssetOverview from "./components/AssetOverview";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import './App.css';
 
-function App() {
-
+function App()
+{
       return (
         <div className="App">
             <Navbar />
