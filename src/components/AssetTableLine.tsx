7ea161625f2da5b438b8013f3c8e6c9e1cf8cfc2
Frédéric Darré <17145256+fdarre@users.noreply.github.com>
2023-03-10 10:47:32 +0100

    Handles asset quantity change

--- src/components/AssetTableLine.tsx
+++ src/components/AssetTableLine.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
@@ -11,23 +11,70 @@ interface IProps {
 
 const AssetTableLine: FunctionComponent<IProps> = ( {name, symbol, price}) => {
 
+    //States
+    const [quantity, setQuantity] = useState<number>(0);
+    const [totalValue, setTotalValue] = useState<number>(0);
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    //Behaviors
+    function calculateTotalValue(price: number, quantity: number): number {
+        return price * quantity;
+    }
+
+    function handleEditClick() {
+        setEditMode(true);
+    }
+
+    function handleSaveClick() {
+        setEditMode(false);
+    }
+
+    function handleCancelClick() {
+        setEditMode(false);
+    }
+
+    function handleQuantityChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = parseInt(e.target.value);
+        setQuantity(value);
+        setTotalValue(calculateTotalValue(price, value));
+    }
+
+
+    //Render
     return (
         <tr>
             <td className="text-truncate">{name}</td>
             <td>{symbol}</td>
             <td className="text-truncate">{price} USDT</td>
-            <td>2</td>
-            <td>45000 USDT</td>
-            <td>19500 USDT</td>
+
+            {/*Display the quantity input if editMode is true, otherwise display the quantity*/}
+            {editMode ? (
+                <td>
+                    <input type="number" value={quantity} onChange={handleQuantityChange} />
+                </td>
+            ) : (
+                <td>{quantity}</td>
+            )}
+            <td>{totalValue}</td>
+            <td>TBI</td>
             <td className="text-center">
-                <div className="btn-group" role="group">
-                    <button className="btn btn-primary" type="button">
+                {editMode ? (
+                    <div className="btn-group" role="group">
+                        <button className="btn btn-success" type="button" onClick={handleSaveClick}>
+                            Save
+                        </button>
+                        <button className="btn btn-secondary" type="button" onClick={handleCancelClick}>
+                            Cancel
+                        </button>
+                    </div>
+                ) : (
+                    <button className="btn btn-primary" type="button" onClick={handleEditClick}>
                         <FontAwesomeIcon icon={faEdit} />
                     </button>
-                    <button className="btn btn-danger" type="button">
-                        <FontAwesomeIcon icon={faTrashAlt} />
-                    </button>
-                </div>
+                )}
+                <button className="btn btn-danger" type="button">
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
             </td>
         </tr>
     );
